be0c6a465c67db4be3ab2b2a3a5759e155878c6d
pranav2004-ops <pranav2904kashyap@gmail.com>
2026-04-03 19:39:47 +0530

    Add main components for task management interface

--- app/index.tsx
+++ app/index.tsx
@@ -1,1 +1,49 @@
- 
+ import DateSelector from "@/components/DateSelector";
+import FilterTabs from "@/components/FilterTabs";
+import Header from "@/components/Header";
+import TaskCard from "@/components/TaskCard";
+import Colors from "@/constants/Colors";
+import { FilterOptions, TASKS } from "@/constants/tasks";
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { FlatList, StyleSheet, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+const Index = () => {
+  const insets = useSafeAreaInsets();
+  const [activeFilter, setActiveFilter] = useState<FilterOptions>("All");
+  return (
+    <View style={[styles.container, { paddingTop: insets.top }]}>
+      <StatusBar style="light" />
+      <FlatList
+        data={TASKS}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <TaskCard task={item} />}
+        ListHeaderComponent={
+          <>
+            {/* Header */}
+            <Header />
+            {/* DateSelector */}
+            <DateSelector />
+            {/* FiltersTabs */}
+            <FilterTabs selected={activeFilter} onSelect={setActiveFilter} />
+          </>
+        }
+        contentContainerStyle={styles.list}
+        showsVerticalScrollIndicator={false}
+      />
+    </View>
+  );
+};
+
+export default Index;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.background,
+  },
+  list: {
+    paddingBottom: 24,
+  },
+});
